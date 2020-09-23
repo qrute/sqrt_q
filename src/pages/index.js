5e92bffb8fdf90c91da00863e6766e6f5611bb70
sqrt_q <nonono36no@ezweb.ne.jp>
2020-09-23 17:31:43 +0900

    add new file

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ class BlogIndex extends React.Component {
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title="" />
         <Helmet>
-          <link rel="canonical" href="https://sqrt_q.com" />
+          <link rel="canonical" href="https://sqrt-q.com" />
         </Helmet>
         <HomeJsonLd />
         <CategoryMenu location={location} />
